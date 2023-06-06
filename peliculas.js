1b9d90c0f73a45553e91ea427bb609670c31d8aa
Marsh1100 <margie.b.agu@gmail.com>
2023-06-06 01:08:44 -0500

    feat: :construction: Uso de map
    Utilizando los métodos de map

--- peliculas.js
+++ peliculas.js
@@ -20,9 +20,69 @@ let imagen = document.getElementById('imagen');
 //Formulario
 let form = document.getElementById('nueva-pelicula');
 
-let peliculas = [];
+let minions = new Map([["titulo","Los minions"],["genero","Animada"],["duracion","2h 15m"],["director","Juan Martínez"],["imagen","https://es.web.img3.acsta.net/pictures/15/02/04/12/29/183973.jpg"]]);
+
+let frozen = new Map([["titulo","Frozen"],["genero","Animada"],["duracion","1h 48m"],["director","Jennifer Lee, Chris Buck"],["imagen","https://lumiere-a.akamaihd.net/v1/images/p_frozen_18373_3131259c.jpeg?region=0%2C0%2C540%2C810"]]);
+
+let peliculas = new Map([["peli0",minions],["peli1",frozen]]);
+
 let posicion;
+
 form.addEventListener('submit',function(e){
+    e.preventDefault(); // Evitar el envío del formulario
+    e.stopPropagation();
+
+    let namePeli ="peli"+String(peliculas.size);
+    console.log(namePeli);
+
+    let pelicula = new Map();
+    pelicula.set("titulo",titulo.value);
+    pelicula.set("genero",genero.value);
+    pelicula.set("duracion",duracion.value);
+    pelicula.set("director",director.value);
+    pelicula.set("imagen",imagen.value);
+
+    peliculas.set(namePeli,pelicula)
+    
+    form.reset();
+    document.getElementById('agregar-peli').style.display='none';
+
+    mostrarPeliculas();
+
+});
+
+function mostrarPeliculas(){
+    peliculas.forEach(function(e,index){
+
+        console.log(e)
+        let idd = index;
+        console.log((idd))
+        let nuevaPelicula = document.createElement("div");
+        nuevaPelicula.setAttribute("class","peliculas");
+        nuevaPelicula.setAttribute("id", index);
+
+        contenedorPeliculas.appendChild(nuevaPelicula);
+
+        document.getElementById(`${index}`).innerHTML=`
+        <h4>${e.get("titulo")}</h4>
+        <img type="image" src="${e.get("imagen")}" class="imagenes">
+        <div class="info-peliculas" >
+            <p><b>Género:</b>${e.get("genero")}</p>
+            <p><b>Duración:</b>${e.get("duracion")}</p>
+            <p><b>Director</b>${e.get("director")}</p>
+            <button class="bi bi-trash-fill" id="${idd}"></button>
+        </div>` 
+    });
+        
+};
+
+
+//form asignado en el HTML las nuevas pelis
+
+/* 
+    let peliculas = [];
+    let posicion;
+    form.addEventListener('submit',function(e){
     e.preventDefault(); // Evitar el envío del formulario
     e.stopPropagation();
 
@@ -50,11 +110,10 @@ form.addEventListener('submit',function(e){
     form.reset();
     document.getElementById('agregar-peli').style.display='none';
 
-});
+}); */
 
-console.log(peliculas)
 
-//Falta mejorar porque no esta recorriendo el mapa
+/* //Falta mejorar porque no esta recorriendo el mapa
 function asignarPeliculas(posicion){
     let idd = "p"+String(posicion);
     console.log(idd);
@@ -73,9 +132,8 @@ function asignarPeliculas(posicion){
 };
 
 
-
+//Borrar sin Map
 document.addEventListener('click',function(event){
-
     console.log(event.target.id)
     try {
         if(document.getElementById(event.target.id).className == 'bi bi-trash-fill'){
@@ -90,13 +148,9 @@ document.addEventListener('click',function(event){
                 
             }
         }
-    } catch (error) {
-        
+    } catch (error) {  
     }
-    
-    
-    
-});
+}); */
 
 
 
